e70355abea98113a4d19216e0ed3c7d6cbcf38db
Raxody <juancho_lopez6@hotmail.com>
2020-11-20 21:46:41 -0500

    agregue la clase diseases

--- src/controlenfermedades/bussines/Diseases.java
+++ src/controlenfermedades/bussines/Diseases.java
@@ -10,5 +10,33 @@ package controlenfermedades.bussines;
  * @author Felipe
  */
 public class Diseases {
+    private String name;
+    private String symptom;
+    private boolean treatment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public boolean isTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(boolean treatment) {
+        this.treatment = treatment;
+    }
+    
     
 }
